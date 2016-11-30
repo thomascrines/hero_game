20a1fbc76834b67eb1f0b8d8cc909043783823cd
Thomas Crines <thomascrines@hotmail.com>
2016-11-30 18:24:28 +0000

    superpower test

---
+++ specs/superpower_spec.js
@@ -0,0 +1,22 @@
+var Superpower = require('../superpower');
+var assert = require("assert");
+
+
+
+describe("superpower", function() {
+
+  var superpower;
+
+  beforeEach(function() {
+   superpower = new Superpower('The Figure Four', 10);
+  });
+
+  it("has name", function() {
+    assert.equal('The Figure Four', superpower.name);
+  });
+
+  it("has strength", function() {
+    assert.equal(10, superpower.strength);
+  });
+
+});
---
+++ superpower.js
@@ -0,0 +1,6 @@
+var Superpower = function(name, strength) {
+  this.name = name;
+  this.strength = strength;
+  };
+
+  module.exports = Superpower;
